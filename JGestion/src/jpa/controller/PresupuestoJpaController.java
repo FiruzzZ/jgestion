174faf054ddf29bb82e3fe920fb2ad82d68f1e1d
romerojoseluiz@gmail.com <romerojoseluiz@gmail.com@d6964222-885f-40d3-92be-45796ae02e8b>
2014-02-25 21:32:16 +0000

    presupuesto fixed estandarización de formato de comprobantes: Presupuesto, NotaCredito

--- JGestion/src/jpa/controller/PresupuestoJpaController.java
+++ JGestion/src/jpa/controller/PresupuestoJpaController.java
@@ -33,12 +33,15 @@ public class PresupuestoJpaController extends AbstractDAO<Presupuesto, Integer> 
         presupuesto.setDetallePresupuestoList(new ArrayList<DetallePresupuesto>());
         getEntityManager();
         try {
+            entityManager.getTransaction().begin();
             entityManager.persist(presupuesto);
             entityManager.getTransaction().commit();
+            entityManager.getTransaction().begin();
             for (DetallePresupuesto detalle : toAttach) {
                 detalle.setPresupuesto(presupuesto);
                 entityManager.persist(detalle);
             }
+            entityManager.getTransaction().commit();
         } catch (Exception ex) {
             if (entityManager.getTransaction().isActive()) {
                 entityManager.getTransaction().rollback();
@@ -57,7 +60,7 @@ public class PresupuestoJpaController extends AbstractDAO<Presupuesto, Integer> 
             if (next == null) {
                 next = 1;
             }
-            return next;
+            return next +1;
         } catch (NoResultException e) {
             System.out.println("Pintó el 1er de " + getEntityClass().getSimpleName() + ", Sucursa=" + sucursal.getNombre() + "(" + sucursal.getPuntoVenta() + ")");
             return 1;
